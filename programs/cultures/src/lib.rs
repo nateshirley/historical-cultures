05c34fa77b6d9838c1f30d8ddd7a021b95b72b29
Nathan Shirley <nshirley22@gmail.com>
2022-01-10 11:43:04 -0500

    likes w/ data calc

--- programs/cultures/src/lib.rs
+++ programs/cultures/src/lib.rs
@@ -1,5 +1,6 @@
 use anchor_lang::prelude::*;
-
+use anchor_lang::solana_program;
+use std::convert::TryFrom;
 declare_id!("5prqgWu5iSVS8DvYruAFFCNQjAbg1RvojMCRL6dPSPye");
 
 mod instructions;
@@ -7,6 +8,7 @@ pub mod state;
 pub mod utils;
 
 use instructions::*;
+use state::*;
 
 #[program]
 pub mod cultures {
@@ -51,7 +53,27 @@ pub mod cultures {
         change_audience_stake::handler(ctx, membership_bump, audience_stake_pool_bump, amount)
     }
 
-    pub fn submit_like(ctx: Context<SubmitLike>) -> ProgramResult {
+    pub fn submit_like(ctx: Context<SubmitLike>, like_attr_bump: u8) -> ProgramResult {
+        //make like account that stays alive for 10 days
+        solana_program::program::invoke_signed(
+            &solana_program::system_instruction::create_account(
+                &ctx.accounts.member.key(),
+                &ctx.accounts.like_attribution.key(),
+                calculate_short_term_rent(1, 10),
+                1,
+                &ctx.program_id,
+            ),
+            &[
+                ctx.accounts.member.to_account_info(),
+                ctx.accounts.like_attribution.to_account_info(),
+                ctx.accounts.system_program.to_account_info(),
+            ],
+            &[&[
+                ctx.accounts.membership.key().as_ref(),
+                ctx.accounts.post.key.as_ref(),
+                &[like_attr_bump],
+            ]],
+        )?;
         Ok(())
     }
 
@@ -60,18 +82,39 @@ pub mod cultures {
     //and use the timestamp for each one to get it
     //it's a tradeoff between flexibility and structure
     //is there a way to use a merkle tree to track the likes?
+
+    /*
+    --- calculate short term rent
+    --- how to find accounts that user is eligible to close?
+
+    */
 }
 
+//could pass this in on the client. a bit safer this way tho
+fn calculate_short_term_rent(data_len: usize, num_days: u64) -> u64 {
+    let __anchor_rent = Rent::get().unwrap();
+    let exempt_lamports = Rent::get().unwrap().minimum_balance(data_len);
+    exempt_lamports
+        .checked_mul(num_days)
+        .unwrap()
+        .checked_div(730)
+        .unwrap()
+}
+
+//so here we are just creating a v small account to make sure that posts can't be double liked
 #[derive(Accounts)]
+#[instruction(like_attr_bump: u8)]
 pub struct SubmitLike<'info> {
     pub member: Signer<'info>,
-    pub post: AccountInfo<'info>,
     #[account(
-        init,
-        seeds = [post.key().as_ref()],
-        bump,
-        payer = member,
-        space = 1
+        constraint = membership.authority == member.key()
+    )]
+    pub membership: Account<'info, Membership>,
+    #[account(mut)]
+    pub post: AccountInfo<'info>, //change to post once i get there
+    #[account(mut,
+        seeds = [membership.key().as_ref(), post.key().as_ref()],
+        bump = like_attr_bump
     )]
     pub like_attribution: AccountInfo<'info>,
     pub system_program: Program<'info, System>,
